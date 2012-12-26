4906b5bf8f7ce07004c926b089f2d5efef2291ef
orzubalsky <juviley@gmail.com>
2012-12-25 20:14:47 -0500

    removed admin dirs from TEPLATE_DIR, was breaking the frontend site

--- ts/settings.py
+++ ts/settings.py
@@ -93,8 +93,6 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    PROJECT_DIR + '/templates/admin',        
-    PROJECT_DIR + '/apps/tradeschool/templates/admin',    
     PROJECT_DIR + '/apps/tradeschool/templates',
     PROJECT_DIR + '/templates',
 )
