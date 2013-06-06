eea86a87bb8f9877dd0004470251c0c7abbf77c4
orzubalsky <juviley@gmail.com>
2013-06-06 12:38:17 -0400

    admin enhancer static files are loaded properly.

--- ts/settings/base.py
+++ ts/settings/base.py
@@ -41,7 +41,8 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(PROJECT_DIR, 'static/'), 
-    os.path.join(PROJECT_DIR, 'apps', 'branches')
+    os.path.join(PROJECT_DIR, 'apps', 'branches'),
+    os.path.join(PROJECT_DIR, '..', 'parts', 'django-admin-enhancer', 'admin_enhancer', 'static')
 )
 
 # List of finder classes that know how to find static files in
