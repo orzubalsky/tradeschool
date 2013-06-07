0c3caab7fd30a93e5cbb83360a55c43e81bdb8f2
orzubalsky <juviley@gmail.com>
2013-06-07 00:53:14 -0400

    organized test files.

---
+++ ts/apps/tradeschool/tests/__init__.py
@@ -0,0 +1,3 @@
+from tradeschool.tests.branch import *
+from tradeschool.tests.schedule import *
+from tradeschool.tests.registration import *
--- ts/apps/tradeschool/tests/branch.py
+++ ts/apps/tradeschool/tests/branch.py
@@ -1,10 +1,3 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 from django.test.client import Client
 from django.core.urlresolvers import reverse
@@ -208,34 +201,3 @@ class BranchSetupTestCase(TestCase):
 
         if os.path.exists(directory):
             shutil.rmtree(directory)
-                
-
-
-"""
-class RegistrationTestCase(TestCase):
-    def setUp(self):
-        self.client = Client()
-
-        self.site     = Site(domain="http://test.tradeschool.coop/", name="test ts", id=2)
-        self.site.save()
-        
-        self.branch   = Branch(title="test trade school", phone="123-123-1234", city="testown", country="US", slug="test", email="test@ts.coop", timezone="America/New_York", site=self.site)
-        self.branch.save()
-        
-        self.venue    = Venue(title="test venue", phone="234-234-2345", city="testown", country="US", venue_type=0, address_1="123 test st", capacity=20, site=self.site)
-        self.venue.save()
-        
-        self.teacher  = Person(fullname="test teacher", email="test@teacher.com", phone="123-123-1234", bio="test bio", website="http://test.com", slug="testteacher")
-        self.teacher.save()
-        
-        self.course   = Course(teacher=self.teacher, category=0, max_students=20, title="test course", slug="testcourse", description="this a test class")
-        self.course.save()
-        
-        self.schedule = Schedule(start_time=datetime(2020, 1, 31, 18, 00, 00), end_time=datetime(2020, 1, 31, 21, 00, 00), venue=self.venue, course=self.course, course_status=3, slug="test-course-01")    
-        self.schedule.save()
-    
-    def test_registration(self):
-        response = self.client.get(reverse('schedule-register', kwargs={ 'schedule_slug': self.schedule.slug }))        
-        print response
-        self.assertEqual(1 + 1, 2)
-"""
