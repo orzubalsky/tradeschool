279c1a9c64b7930910989c8bebb6090854d15b4b
orzubalsky <juviley@gmail.com>
2013-07-20 17:54:38 -0400

    fabric deployment configuration

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,12 @@
 from fabric.api import env, task, sudo, prompt, cd, put, puts
 from fabric.contrib.files import upload_template
+from fab_config import Config
 
-env.hosts = [ '198.199.64.223', ]
+env.hosts            = Config.hosts
+fab_username         = Config.fab_username
+fab_password         = Config.fab_password
+server_settings_file = Config.server_settings_file 
+apache_conf_file     = Config.apache_conf_file
 
 #####
 #
@@ -11,37 +16,43 @@ env.hosts = [ '198.199.64.223', ]
 
 @task
 def update_sourcecode():
-    with cd('/opt/projects/ts/'):
-        sudo('git pull',user='tradeschooler')
+    with cd('/opt/projects/tse/'):
+        sudo('git pull',user=fab_username)
 
 @task
 def update_project_settings():
     filename = prompt( 'Enter name of local settings file:',
-                       default='ts/settings/server.py' )
-    destination = '/opt/projects/ts/settings/server.py'
+                       default=server_settings_file )
+    destination = '/opt/projects/tse/ts/settings/server.py'
     put(filename,destination,use_sudo=True)
-    sudo('chown tradeschooler:webdev %s' % destination)
+    sudo('chown %s:webdev %s' % (fab_username, destination))
 
 @task
 def run_buildout():
-    with cd('/opt/projects/ts/'):
-        sudo('./bin/buildout -c server.cfg',user='tradeschooler')
+    with cd('/opt/projects/tse/'):
+        sudo('./bin/buildout -v -c server.cfg',user=fab_username)
 
 @task
 def update_db():
-    with cd('/opt/projects/ts/'):
-        sudo('./bin/django syncdb',user='tradeschooler')
-        sudo('./bin/django migrate',user='tradeschooler')
+    with cd('/opt/projects/tse/'):
+        sudo('./bin/django syncdb', user=fab_username)
+        sudo('./bin/django migrate tradeschool', user=fab_username)
     
 @task
 def update_static_files():
     # run the django command to update static files
-    # then push them all to rackspace
-    pass
+    with cd('/opt/projects/tse/'):
+        sudo('./bin/django collectstatic', user=fab_username)    
+
+@task 
+def load_fixtures():
+    # load fixtures
+    with cd('/opt/projects/tse/'):
+        sudo('./bin/django loaddata email_initial_data.json', user=fab_username)
 
 @task
 def restart_wsgi():
-    with cd('/opt/projects/ts'):
+    with cd('/opt/projects/tse'):
         sudo('touch bin/django.wsgi')
 
 @task
@@ -73,65 +84,77 @@ def deploy():
 def init_os_package_setup():
     sudo('apt-get -y update')
     sudo('apt-get -y upgrade')
-    sudo('apt-get install git python-dev libjpeg62 libjpeg-dev libfreetype6 libfreetype6-dev zlib1g-dev ffmpeg')
-    sudo('apt-get install postgresql-9.1 postgresql-server-dev-9.1')
-    sudo('apt-get install binutils libproj-dev gdal-bin libgeoip1 python-gdal python-psycopg2')
-    sudo('apt-get install postgresql-9.1-postgis')
+    sudo('apt-get install git python-dev')
+    sudo('apt-get install mysql-server mysql-client libmysqlclient-dev')
     sudo('apt-get install apache2 libapache2-mod-wsgi')
+    sudo('apt-get install gettext memcached')
 
 @task
-def init_tradeschooler_user():
+def init_fab_user():
     sudo('groupadd webdev')
-    sudo('useradd -G postgres,webdev --create-home --shell /bin/bash tradeschooler')
-    sudo('passwd tradeschooler')
+    sudo('useradd -G mysql,webdev --create-home --shell /bin/bash %s' % fab_username)
+    sudo('passwd %s' % fab_password)
 
-    sudo('ssh-keygen -t rsa -C "tradeschooler@tradeschool.coop"',user='tradeschooler')
-    puts('Created the following id_rsa.pub file for user tradeschooler:')
-    sudo('cat /home/tradeschooler/.ssh/id_rsa.pub')
+    sudo('ssh-keygen -t rsa -C "fab@tradeschool.coop"',user=fab_username)
+    puts('Created the following id_rsa.pub file for user %s:' % fab_username)
+    sudo('cat /home/%s/.ssh/id_rsa.pub' % fab_username)
     prompt('Please upload this to github as a "deploy key" (https://github.com/orzubalsky/tradeschool/settings/keys).\n' \
                'When done, press enter to continue.')
 
 @task
 def init_project_sourcecode():
-    sudo('mkdir --parents /opt/projects/ts')
-    sudo('chown urtbot:webdev /opt/projects/ts')
-    with cd('/opt/projects/ts'):
-        sudo('git clone git@github.com:orzubalsky/tradeschool.git .',user='tradeschooler')
+    sudo('mkdir --parents /opt/projects/tse')
+    sudo('chown %s:webdev /opt/projects/tse' % fab_username)
+    with cd('/opt/projects/tse'):
+        sudo('git clone git@github.com:orzubalsky/tradeschool.git .',user=fab_username)
 
 @task
 def init_buildout():
-    with cd('/opt/projects/ts'):
-        sudo('python bootstrap.py -c server.cfg',user='tradeschooler')
+    with cd('/opt/projects/tse'):
+        sudo('python bootstrap.py -v 2.1.1 -c server.cfg',user=fab_username)
 
 @task
-def init_postgres_db():
-    sudo('chown postgres /opt/projects/urt/trunk/urt/etc/sh/*')
-    sudo('chmod u+x /opt/projects/urt/trunk/urt/etc/sh/*')
-    sudo('/opt/projects/ts/etc/sh/create_template_postgis-debian.sh',user='postgres')
-    sudo('createuser tradeschooler',user='postgres')
-    sudo('createdb --template=template_postgis --owner=tradeschooler tradeschool',user='postgres')
+def init_mysql_db():
+    db_name = prompt( 'Enter name of database:',
+                       default='tradeschool_test' )    
+    #sudo('mysqladmin create %s -u root' % db_name, user=fab_username)
+    
+    db_user = prompt( 'Enter name of database user:',
+                       default='tradeschooler' )
+    db_password = prompt( 'Enter password:')
+    
+    sudo('mysql_install_db', user=fab_username)
+    sudo('/usr/bin/mysql_secure_installation', user=fab_username)
+        
+    sudo('mysql -u root CREATE USER %s@localhost IDENTIFIED BY %s;' % (db_user, db_password))                      
+    sudo('mysql -u root GRANT ALL PRIVILEGES ON %s.* TO %s@localhost;' % (db_name, db_user))
+
+@task
+def create_cache_folder():
+    with cd('/opt/projects/tse'):
+        sudo('mkdir tmp',user=fab_username)    
+
 
 @task
 def init_apache():
     filename = prompt( 'Enter name of local apache conf file:',
-                       default='ts/etc/conf/apache.conf' )
-    destination = '/etc/apache2/sites-available/tradeschool.coop'
+                       default=apache_conf_file )
+    destination = '/etc/apache2/sites-available/tstest.net'
     put(filename,destination,use_sudo=True)
 
-    sudo('a2ensite tradeschool.coop')
+    sudo('a2ensite tstest.net')
     sudo('service apache2 reload') # do we need this line??
 
 @task
 def initialize_everything():
     init_os_package_setup()
-    init_tradeschooler_user()
+    init_fab_user()
     init_project_sourcecode()
     update_project_settings()
     init_buildout()
-    run_buildout()
+    init_mysql_db()
 
-    init_postgres_db()
+    run_buildout()
     update_db()
 
     init_apache()
-
