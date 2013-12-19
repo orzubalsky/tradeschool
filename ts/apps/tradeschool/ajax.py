07189dfb02e7b4c248c64f22ac0311162c7f9263
orzubalsky <juviley@gmail.com>
2013-12-18 19:50:10 -0500

    syntax edits

--- ts/apps/tradeschool/ajax.py
+++ ts/apps/tradeschool/ajax.py
@@ -5,11 +5,18 @@ from tradeschool.views import *
 
 @dajaxice_register(method='POST')
 def schedule_load_form(request, branch_slug=None, schedule_slug=None):
-    """ """
+    """
+    """
     return schedule_register(request, branch_slug, schedule_slug)
-    
-    
+
+
 @dajaxice_register(method='POST')
 def schedule_submit_form(request, data, branch_slug=None, schedule_slug=None):
-    """ """
-    return schedule_register(request, branch_slug, schedule_slug, deserialize_form(data))
+    """
+    """
+    return schedule_register(
+        request,
+        branch_slug,
+        schedule_slug,
+        deserialize_form(data)
+    )
