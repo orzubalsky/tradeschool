7d88f79df1f130e909c8d6bbdadc18fe43a677ac
orzubalsky <juviley@gmail.com>
2013-12-18 21:59:45 -0500

    syntax edits

--- ts/apps/tradeschool/templatetags/smartdate.py
+++ ts/apps/tradeschool/templatetags/smartdate.py
@@ -4,13 +4,15 @@ import re
 
 register = template.Library()
 
-# --------------------------------------------------------------------------------
-#   |smartdate:"date format" -- new arg 'c' (change) alteras the AM/pm appearance
-# --------------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# |smartdate:"date format" -- new arg 'c' (change) alteras the AM/pm appearance
+# -----------------------------------------------------------------------------
 @register.filter
 def smartdate(value, arg):
     rendered = date_filter(value, arg)
     if 'c' in arg:
-        rendered = re.sub('(a|p)\.m\.c', lambda m: '%sm' % m.group(1), rendered)
+        rendered = re.sub(
+            '(a|p)\.m\.c', lambda m: '%sm' % m.group(1), rendered)
         rendered = re.sub('(A|P)Mc', lambda m: '%s.M.' % m.group(1), rendered)
     return rendered
