77174a588ec6ac9dab528f17e2f7d1ba20447069
orzubalsky <juviley@gmail.com>
2013-12-28 17:04:29 -0500

    trying to fix url loading

--- ts/apps/tradeschool/middleware.py
+++ ts/apps/tradeschool/middleware.py
@@ -7,49 +7,52 @@ from tradeschool.models import *
 class BranchMiddleware(object):
     def process_request(self, request):
 
-        url = resolve(request.path)
+        try:        
+            url = resolve(request.path)
 
-        # normal requests to branch URLs will have a branch_slug URL param
-        branch_slug = url.kwargs.get('branch_slug')
+            # normal requests to branch URLs will have a branch_slug URL param
+            branch_slug = url.kwargs.get('branch_slug')
 
-        # ajax requests, however, contain the branch_slug in the the POST data,
-        # as part of the Dajaxice arguments
-        if (branch_slug is None and
-           request.method == 'POST' and
-           request.is_ajax()):
-
-            # the incoming data from Dajaxice looks like a Python dictionary,
-            # but it's a string. We're parsing it using the ast library
-            ajax_dict = ast.literal_eval(request.POST.dict()['argv'])
-
-            # after that we can get the branch_slug value,
-            # like from any other dictionary
-            branch_slug = ajax_dict['branch_slug']
-
-        try:
+            # ajax requests, however, contain the branch_slug in the the POST data,
+            # as part of the Dajaxice arguments
             if (branch_slug is None and
-               (url.app_name == 'admin' or url.app_name == 'rosetta') and
-               request.user.is_staff):
+               request.method == 'POST' and
+               request.is_ajax()):
 
-                # translate the admin & rosetta backends
-                # to the language set by the logged in user
-                translation.activate(request.user.language)
+                # the incoming data from Dajaxice looks like a Python dictionary,
+                # but it's a string. We're parsing it using the ast library
+                ajax_dict = ast.literal_eval(request.POST.dict()['argv'])
 
-                if request.user.branches.count() > 0:
-                    if request.user.default_branch is not None:
-                        branch = request.user.default_branch
+                # after that we can get the branch_slug value,
+                # like from any other dictionary
+                branch_slug = ajax_dict['branch_slug']
+
+            try:
+                if (branch_slug is None and
+                   (url.app_name == 'admin' or url.app_name == 'rosetta') and
+                   request.user.is_staff):
+
+                    # translate the admin & rosetta backends
+                    # to the language set by the logged in user
+                    translation.activate(request.user.language)
+
+                    if request.user.branches.count() > 0:
+                        if request.user.default_branch is not None:
+                            branch = request.user.default_branch
+                        else:
+                            branch = Branch.objects.filter(
+                                organizers=request.user)[0]
+                        timezone.activate(branch.timezone)
                     else:
-                        branch = Branch.objects.filter(
-                            organizers=request.user)[0]
-                    timezone.activate(branch.timezone)
-                else:
-                    pass
+                        pass
 
-            branch = Branch.objects.get(slug=branch_slug)
-            timezone.activate(branch.timezone)
+                branch = Branch.objects.get(slug=branch_slug)
+                timezone.activate(branch.timezone)
 
-            # translate the frontend to the language set in the branch
-            translation.activate(branch.language)
+                # translate the frontend to the language set in the branch
+                translation.activate(branch.language)
 
-        except Branch.DoesNotExist:
+            except Branch.DoesNotExist:
+                pass
+        except ImproperlyConfigured:
             pass
