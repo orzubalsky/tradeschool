e1a3f7111657fae5f832f7e928aa50158c30ed3b
orzubalsky <juviley@gmail.com>
2014-01-26 15:42:16 -0500

    adding branch to venue form

--- ts/apps/tradeschool/admin.py
+++ ts/apps/tradeschool/admin.py
@@ -945,7 +945,7 @@ class VenueAdmin(BaseAdmin):
         }),
         # Translators: This is the a header in the branch admin form
         (_('Additional Info'), {
-            'fields': ('capacity', 'resources' 'branch',)
+            'fields': ('capacity', 'resources', 'branch',)
         }),
     )
     actions = [
