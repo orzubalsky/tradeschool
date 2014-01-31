92aef1b9f6879dd848866005ed0d9e2302de905b
orzubalsky <juviley@gmail.com>
2014-01-31 08:21:11 -0500

    page requests that end with a slash are fixed

--- ts/apps/tradeschool/urls.py
+++ ts/apps/tradeschool/urls.py
@@ -18,5 +18,6 @@ urlpatterns = patterns(
     url(r'class/$', 'redirect_to_course_list', name='redirect-to-course-list'),
     url(r'class$', 'redirect_to_course_list', name='redirect-to-course-list'),
     url(r'page/(?P<url>.*)$', 'branch_page', name='branch-page'),
+    url(r'page/(?P<url>.*)/$', 'branch_page', name='branch-page'),
     url(r'$', 'course_list', name='course-list'),
 )
