2a7d2f5f79a184e3efd0c199ffc566cc7b6c97b5
orzubalsky <juviley@gmail.com>
2014-02-25 10:37:43 -0500

    scrolling navigation for story page

--- ts/apps/tradeschool/static/js/page/story.js
+++ ts/apps/tradeschool/static/js/page/story.js
@@ -1,23 +1,31 @@
 ;(function($){
 var page = window.page = new function() 
 {
+    this.last_scroll_value;
+    this.total_height;
+    this.is_scrolling = false;
+    this.ignore_scroll_event = false;
     this.page_height;
     this.video_height;
+    this.$pages;
     this.current_page = 0;
     this.total_pages;
 
     this.init = function() 
     {
-        this.page_height = $('#container').outerHeight(true);
+        this.$pages = $('.section');
+        this.page_height = $('#container').outerHeight(true) - 50;
         this.video_height = $('#video').outerHeight(true);
-        this.total_pages = $('.section').size() - 1;
-        
+        this.total_pages = this.$pages.size() - 1;
+        this.last_scroll_value = Math.round($(document).scrollTop());
+        this.total_height = $(document).height();
+
         $('.section').css({'height': this.page_height + 'px'});
 
+        this.navigation_buttons_display(this.last_scroll_value);
         this.navigation();
-        this.scrolls();
+        this.set_scroll_functionality();
     };
-
 
     this.navigation = function() 
     {
@@ -29,7 +37,9 @@ var page = window.page = new function()
             
             // console.log('preivous');
 
-            self.previous_page();
+            self.ignore_scroll_event = true;
+
+            self.scroll_to(self.get_target_scroll_position(false));
         });
 
         $('#next').on('click', function(e)
@@ -37,69 +47,124 @@ var page = window.page = new function()
             e.preventDefault();
             
             // console.log('next');
+            // console.log(self.last_scroll_value);
 
-            self.next_page();            
+            self.ignore_scroll_event = true;
+
+            self.scroll_to(self.get_target_scroll_position(true));          
         });
-
-        // $(window).on('mousewheel', function(e)
-        // {
-        //     if (e.deltaY > 0)
-        //     {
-        //         // console.log('up');
-        //         self.previous_page();
-        //     }
-        //     else
-        //     {
-        //         // console.log('down');
-        //         self.next_page();
-        //     }
-        // }); 
-
     };
 
-    this.previous_page = function()
+    this.set_scroll_functionality = function()
     {
         var self = this;
 
-        if (self.current_page > 0)
+        $(window).scroll(function(scroll_event)
         {
-            var top = $('#content').position().top;        
-
-            var scroll_to = (self.current_page == 4) ? top + self.video_height : top + self.page_height;
-
-            $('#content').animate({ 'top': scroll_to + 'px' }, 100, function()
+            if (self.is_scrolling)
             {
-                self.current_page--;
+                clearTimeout(self.is_scrolling);
+            }
+            
+            self.is_scrolling = setTimeout(function()
+            { 
+                if (self.ignore_scroll_event)
+                {
+                    self.ignore_scroll_event = false;
 
-                (self.current_page == 0) ? $('#previous').hide() : $('#previous').show();
-                (self.current_page == self.total_pages) ? $('#next').hide() : $('#next').show();                
-            });
-        }
+                    return false;
+                }
+                else
+                {
+                    self.after_scroll_callback(scroll_event);
+                }
+            }, 400);
+        });        
     };
 
-    this.next_page = function()
+    this.after_scroll_callback = function(e)
     {
-        var self = this;
-
-        if (self.current_page < self.total_pages)
-        {
-            var top = $('#content').position().top;        
-
-            var scroll_to = (self.current_page == 2) ? top - self.video_height : top - self.page_height;
-
-            $('#content').animate({ 'top': scroll_to + 'px' }, 100, function()
-            {
-                self.current_page++;
-
-                (self.current_page == 0) ? $('#previous').hide() : $('#previous').show();
-                (self.current_page == self.total_pages) ? $('#next').hide() : $('#next').show();                
-            });
-        }
-    };
-
-    this.scrolls = function()
-    {
+        // console.log('after_scroll_callback');
         
+        var self = this;
+
+        self.ignore_scroll_event = true;
+        
+        var current_scroll_position = $(document).scrollTop();
+
+        var scroll_down = (current_scroll_position > self.last_scroll_value) ? true : false;
+
+        self.scroll_to(self.get_target_scroll_position(scroll_down));
+    };
+
+    this.scroll_to = function(top_value)
+    {
+        // console.log('scroll_to');
+
+        var self = this;
+
+        var target_scroll_position = Math.round(top_value);
+
+        $('html, body').animate(
+        {
+            scrollTop: target_scroll_position
+        }, 100, function()
+        {
+            self.last_scroll_value = target_scroll_position;
+
+            self.navigation_buttons_display(target_scroll_position); 
+        });
+    };
+
+    this.navigation_buttons_display = function(scroll_position)
+    {
+        var self = this;
+
+        (scroll_position <= 0) ? $('#previous').hide() : $('#previous').show();
+        (scroll_position >= self.total_height - 2 * self.page_height) ? $('#next').hide() : $('#next').show();   
+    };
+
+    this.get_target_scroll_position = function(scroll_down)
+    {
+        // console.log('get_target_scroll_position');
+
+        var self = this;
+
+        var current_scroll_position = Math.round($(document).scrollTop());
+
+        var target_scroll_position;
+
+        if (scroll_down)
+        {
+            for (var i=self.total_pages; i>=0; i--)
+            {
+                var page_top = Math.round(self.$pages.eq(i).position().top);
+
+                // console.log('page_top: ' + page_top);
+
+                if (page_top > current_scroll_position)
+                {
+                    target_scroll_position = page_top;
+                    // console.log('setting target_scroll_position to: ' + target_scroll_position);                    
+                }
+            }            
+        }
+        else
+        {
+            for (var i=0; i<self.total_pages; i++)
+            {
+                var page_top = Math.round(self.$pages.eq(i).position().top);
+
+                if (page_top < current_scroll_position)
+                {
+                    target_scroll_position = page_top;
+                }
+            }
+        }
+        // console.log('target_scroll_position: ' + target_scroll_position);
+
+        return Math.round(target_scroll_position);
+
     };
 
 };
