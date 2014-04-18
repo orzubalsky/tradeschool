a9b0af866fe9145348423ba885d0a3ae72f9fb0b
Daniel Nephin <dnephin@gmail.com>
2014-04-18 15:39:52 -0400

    Remove trailing whitespace and fix start a tradeschool link.

--- ts/urls.py
+++ ts/urls.py
@@ -29,7 +29,7 @@ urlpatterns += patterns('',
 urlpatterns += patterns('',
     url(r'^tinymce/', include('tinymce.urls')),
 )
-    
+
 # dajaxice urls
 urlpatterns += patterns('',
     url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
@@ -44,18 +44,18 @@ urlpatterns += patterns('django.contrib.flatpages.views',
 urlpatterns += staticfiles_urlpatterns()
 
 # serve static files from media directory when in debug mode
-if settings.DEBUG: 
+if settings.DEBUG:
     urlpatterns += patterns('',
         url(r'^media/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': settings.MEDIA_ROOT, }),
    )
-   
+
 # tradeschool app urls
 urlpatterns += patterns('',
     url(r'^u/sdb/', 'tradeschool.views.switch_default_branch', name='switch-default-branch'),
     url(r'^start-a-tradeschool/', 'tradeschool.views.start_a_tradeschool', name='start-a-tradeschool'),
-    url(r'^story/', 'tradeschool.views.story', name='story'),    
+    url(r'^story/', 'tradeschool.views.story', name='story'),
     url(r'^school-submitted/(?P<slug>[0-9A-Za-z\-_]+)', 'tradeschool.views.branch_submitted', name='branch-submitted'),
     url(r'^cluster/(?P<slug>[0-9A-Za-z\-_]+)/', 'tradeschool.views.cluster_list', name='cluster-list'),
     url(r'^(?P<branch_slug>[0-9A-Za-z\-_]+)/', include('tradeschool.urls')),
-    url(r'^$', 'tradeschool.views.branch_list', name='branch-list'),    
-)    
+    url(r'^$', 'tradeschool.views.branch_list', name='branch-list'),
+)
