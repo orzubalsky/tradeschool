a60dcbe1b8d283f951f65f8feba1dc355801edda
orzubalsky <juviley@gmail.com>
2014-06-12 17:36:04 -0400

    reverting changes

--- ts/apps/tradeschool/calendar/export.py
+++ ts/apps/tradeschool/calendar/export.py
@@ -10,36 +10,37 @@ def _build_uid_for_course(course):
     """Construct an event UID from a :class:`tradeschool.models.Course`.
     """
     return "{label}-{id}@{site}".format(
-            label=course.slug,
-            id=course.id,
-            site=course.branch.site.domain)
+        label=unicode(course.slug, "utf-8"),
+        id=course.id,
+        site=course.branch.site.domain)
 
 
 def _build_location_for_venue(venue):
     return "{title}, {address} {city}, {state}, {country}".format(
-            title=venue.title,
-            address=venue.address_1,
-            state=venue.state,
-            city=venue.city,
-            country=venue.country)
+        title=unicode(venue.title, "utf-8"),
+        address=unicode(venue.address_1, "utf-8"),
+        state=unicode(venue.state, "utf-8"),
+        city=unicode(venue.city, "utf-8"),
+        country=unicode(venue.country, "utf-8")
+    )
 
 
 def course_to_event(course):
     """Convert a Course into a icalendar event.
-    
+
     :param course: a :class:`tradeschool.models.Course` to export as an Event
     :returns: :class:`icalendar.Event`
     """
     return Event(**{
         'uid': vText(_build_uid_for_course(course)),
         'created': vDatetime(course.created),
-        'description': vText(course.description),
+        'description': vText(unicode(course.description, "utf-8")),
         'dtstart': vDatetime(course.start_time),
         'dtend': vDatetime(course.end_time),
         'last-mod': vDatetime(course.updated),
         'dtstamp': vDatetime(datetime.datetime.now()),
         'location': vText(_build_location_for_venue(course.venue)),
-        'summary': vText(course.title),
+        'summary': vText(unicode(course.title, "utf-8")),
         'url': vUri(course.course_view_url),
     })
 
