ace1fb813adcf5b713191283739a7b84aa426101
orzubalsky <juviley@gmail.com>
2014-06-12 16:19:32 -0400

    ensuring fields are in unicode

--- ts/apps/tradeschool/calendar/export.py
+++ ts/apps/tradeschool/calendar/export.py
@@ -10,18 +10,18 @@ def _build_uid_for_course(course):
     """Construct an event UID from a :class:`tradeschool.models.Course`.
     """
     return "{label}-{id}@{site}".format(
-            label=course.slug,
+            label=unicode(course.slug),
             id=course.id,
             site=course.branch.site.domain)
 
 
 def _build_location_for_venue(venue):
     return "{title}, {address} {city}, {state}, {country}".format(
-            title=venue.title,
-            address=venue.address_1,
-            state=venue.state,
-            city=venue.city,
-            country=venue.country)
+            title=unicode(venue.title),
+            address=unicode(venue.address_1),
+            state=unicode(venue.state),
+            city=unicode(venue.city),
+            country=unicode(venue.country)
 
 
 def course_to_event(course):
@@ -33,13 +33,13 @@ def course_to_event(course):
     return Event(**{
         'uid': vText(_build_uid_for_course(course)),
         'created': vDatetime(course.created),
-        'description': vText(course.description),
+        'description': vText(unicode(course.description)),
         'dtstart': vDatetime(course.start_time),
         'dtend': vDatetime(course.end_time),
         'last-mod': vDatetime(course.updated),
         'dtstamp': vDatetime(datetime.datetime.now()),
         'location': vText(_build_location_for_venue(course.venue)),
-        'summary': vText(course.title),
+        'summary': vText(unicode(course.title)),
         'url': vUri(course.course_view_url),
     })
 
